81d39c476bb97cf34519ee71697b36184d1cfc3c
Nikolai <nik.vagin1995@mail.ru>
2020-08-06 06:53:55 +0300

    Admin model orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name',
+                    'email', 'adress', 'postal_code',
+                    'city', 'paind', 'created', 'update']
+    list_filter = ['paind', 'created', 'update', 'city']
+    search_fields = ['first_name', 'last_name', 'email', 'city', 'adress', 'postal_code']
+    list_editable = ("paind",)
+    inlines = [OrderItemInline]
