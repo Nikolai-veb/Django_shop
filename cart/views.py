aecaf8f306601727132677027f88cc120aa99376
nkolai <nik.vagin1995@mail.ru>
2020-08-08 22:41:22 +0300

    all settings model orders

--- cart/views.py
+++ cart/views.py
@@ -26,6 +26,7 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     """Oбработчик для страницы списка товаров, добавленных в корзину"""
     cart = Cart(request)
+
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(
              initial={'quantity': item['quantity'],
